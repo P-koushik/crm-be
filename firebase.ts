4db995b6103dc639821494f31fb9b57817660fbc
koushik-0513 <koushik@prodgain.com>
2025-12-12 13:42:13 +0530

    fourth try

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,10 @@
 import admin from "firebase-admin";
-import * as serviceAccount from "./serviceAccountKey.json";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+  credential: admin.credential.cert(process.env.SERVICE_ACCOUNT_KEY_PATH as admin.ServiceAccount)
 });
 
 export default admin;
